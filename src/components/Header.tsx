7973e648bdc1917be41dcbe687389b0884d674c0
Esther <estherlecina@gmail.com>
2025-09-09 13:13:44 +0200

    Add style to header and create menu toggler

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,114 @@
 import { NavLink } from "react-router-dom";
-import "./Header.css"
+import { useState } from "react";
+import "./Header.css";
 import { useLanguage } from "../context/languageContext";
 import { headerContent } from "../data/global";
+import elsImago from "../assets/ELS_imago.svg";
 
 const Header = () => {
+  const { language, setLanguage } = useLanguage();
+  const languageSelector = (e: { target: { value: string } }) => {
+    setLanguage(e.target.value as "en" | "es");
+  };
 
-    const { language, setLanguage } = useLanguage();
-    const languageSelector = (e: { target: { value: string; }; }) => {
-        setLanguage(e.target.value as "en" | "es")
-    }
-
-    return (
-        <header>
-            <div className="logo">
-                <NavLink to="/">EstherLecinaWeb</NavLink>
-            </div>
-            <nav>
-                <ul className="menu-list">
-                    <li><NavLink to="/about">{headerContent.translations[language].navBar.about}</NavLink></li>
-                    <li><NavLink to="/portfolio">Galería</NavLink>
-                        <ul>
-                            <li><NavLink to="/graphic-design">{headerContent.translations[language].navBar.graphicDesign}</NavLink></li>
-                            <li><NavLink to="/illustration">{headerContent.translations[language].navBar.illustrationAndComic}</NavLink></li>
-                            <li><NavLink to="/ux-ui-design">{headerContent.translations[language].navBar.designUxUi}</NavLink></li>
-                            <li><NavLink to="/development">{headerContent.translations[language].navBar.development}</NavLink></li>
-                            <li><NavLink to="/others">{headerContent.translations[language].navBar.others}</NavLink></li>
-                        </ul>
-                    </li>
-                    <li><NavLink to="/contact">{headerContent.translations[language].navBar.contact}</NavLink></li>
-                    <li><NavLink to="/blog">{headerContent.translations[language].navBar.blog}</NavLink></li>
-                </ul>
-            </nav>
-            <div className="language-select">
-                <select value={language} onChange={(e) => languageSelector(e)}>
-                    <option value="en">EN</option>
-                    <option value="es">ES</option>
-                </select>
-            </div>
-        </header>
-    )
-}
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  return (
+    <header>
+      <div className="nav-menu-container">
+        <NavLink to="/">
+          <div className="logo">
+            <img src={elsImago} width={40} height={40} alt="imagotipo de ELW" />
+            <p className="els">
+              EL<span className="colored-w">W</span>
+            </p>
+          </div>
+        </NavLink>
+        <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+          <svg
+            role="button"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
+          </svg>
+        </button>
+      </div>
+      <nav
+        className={`nav-menu-list ${
+          isMenuOpen ? "menu-visible" : ""
+        }`}
+      >
+        <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+          <span className="navbar-toggler-icon">
+            <svg
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+            >
+              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+            </svg>
+          </span>
+        </button>
+        <ul className="menu-list" onClick={toggleMenu}>
+          <li>
+            <NavLink to="/about">
+              {headerContent.translations[language].navBar.about}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/portfolio">Galería</NavLink>
+            <ul>
+              <li>
+                <NavLink to="/graphic-design">
+                  {headerContent.translations[language].navBar.graphicDesign}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/illustration">
+                  {
+                    headerContent.translations[language].navBar
+                      .illustrationAndComic
+                  }
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/ux-ui-design">
+                  {headerContent.translations[language].navBar.designUxUi}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/development">
+                  {headerContent.translations[language].navBar.development}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/others">
+                  {headerContent.translations[language].navBar.others}
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <NavLink to="/contact">
+              {headerContent.translations[language].navBar.contact}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/blog">
+              {headerContent.translations[language].navBar.blog}
+            </NavLink>
+          </li>
+        </ul>
+        <select className="language-selector" value={language} onChange={(e) => languageSelector(e)}>
+          <option value="en">EN</option>
+          <option value="es">ES</option>
+        </select>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
